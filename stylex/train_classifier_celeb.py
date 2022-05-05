52e3f1139390e7ca99906357e361e429512d4269
Ubuntu <ubuntu@ip-172-31-43-200.us-east-2.compute.internal>
2022-05-05 19:54:06 +0000

    use pytorch tutorial

--- stylex/train_classifier_celeb.py
+++ stylex/train_classifier_celeb.py
@@ -10,7 +10,7 @@ import numpy as np
 
 from torch.utils.data import Dataset, DataLoader
 from resnet_classifier import load_resnet_classifier
-
+#torch.cuda.empty_cache()
 device = "cuda:0"
 model_name = "resnet-18-64px-gender.pt"
 print("Cargando model"+str(model_name)+" ...")
@@ -27,10 +27,12 @@ from PIL import Image
 import pandas as pd
 from torchvision import transforms
 from CelebA_utils import get_train_valid_test_dataset
-img_size = 3 #64
+import time
+import copy
+img_size = 256# 3 #64
 celeb_train, celeb_val, celeb_test = get_train_valid_test_dataset("../data/CelebA/celeba-dataset/", "../data/CelebA/celeba-dataset/list_attr_celeba.csv", "Male", image_size=img_size)            
 
-torch.cuda.empty_cache()
+
 batch_size = 128
 cel_train_loader = DataLoader(celeb_train, batch_size=batch_size, pin_memory=True)
 cel_val_loader = DataLoader(celeb_val, batch_size=batch_size)
@@ -75,60 +77,85 @@ def validate_model(model, loader, criterion):
 
     return loss, accuracy
 
-def train_model(model, train_loader, val_loader, optimizer, criterion, test_model=False, epochs=10):
+def train_model(model, train_loader, val_loader, optimizer, criterion, test_model=False, num_epochs=10):    
     """Trains model"""
+    dataloaders = {"train":train_loader,"val":val_loader}
+    since = time.time()
 
-    # Put the model in training mode.
-    model.train()
+    val_acc_history = []
 
-    # For each epoch...
-    for epoch in range(epochs):
-        # For each batch in the training set...
-        for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
-            # Send the data and labels to the device.
-            data, target = data.to(device), target.to(device)
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-            # Zero out the gradients.
-            optimizer.zero_grad()
-            print(data.shape)
-            # Forward pass.
-            output = model(data)
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
 
-            # Calculate the loss.
-            loss = criterion(output, target)
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
 
-            # Backward pass.
-            loss.backward()
+            running_loss = 0.0
+            running_corrects = 0
 
-            # Update the weights.
-            optimizer.step()
+            # Iterate over data.
+            #for inputs, labels in dataloaders[phase]:
+            for batch_idx, (inputs, labels) in enumerate(tqdm(dataloaders[phase])):    
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-            # Print the loss.
-            if batch_idx % 100 == 0:
-                print('Epoch: {}/{}'.format(epoch + 1, epochs),
-                      'Loss: {:.4f}'.format(loss.item()))
-        if((epoch%3)==0):
-            # Validate the model.
-            val_loss, val_acc = validate_model(model, val_loader, criterion)
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-            # Print the validation loss.
-            print('Validation Loss: {:.4f}'.format(val_loss))
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
 
-            # Print the validation accuracy.
-            print('Validation Accuracy: {:.4f}'.format(val_acc))
+                    _, preds = torch.max(outputs, 1)
 
-    if test_model:
-        # Test the model.
-        test_loss, test_acc = validate_model(model, test_loader, criterion)
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
 
-        # Print the test loss.
-        print('Test Loss: {:.4f}'.format(test_loss))
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
-        # Print the test accuracy.
-        print('Test Accuracy: {:.4f}'.format(test_acc))    
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
 
 model.requires_grad_(True)
-train_model(model, cel_train_loader, cel_val_loader, optimizer, criterion, epochs=10)
+train_model(model, cel_train_loader, cel_val_loader, optimizer, criterion, num_epochs=10)
 
 # Test the model.
 test_loss, test_acc = validate_model(model, cel_test_loader, criterion)
